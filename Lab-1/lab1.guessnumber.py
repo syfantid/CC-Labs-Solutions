59534455a479586d62a99163387c603c43c91684
Sofia Yfantidou <syfantid@csd.auth.gr>
2018-03-02 08:52:55 +0100

    Initial version of the game

---
+++ Lab-1/lab1.guessnumber.py
@@ -0,0 +1,26 @@
+import random
+
+# Allows multiple games
+while(True):
+    random_number = random.randint(1, 20)
+
+    # Allows multiple guesses
+    while(True):
+        guess = int(input('Guess the number between 1 and 20: '))
+        if(guess == random_number):
+            print('Excellent guess!')
+            break
+        elif(guess > random_number):
+            print('Sorry, number too high. Try again!')
+        else:
+            print('Sorry, number too low. Try again!')
+
+    # Allow user to play again
+    play_again = input('Do you want to play again (y/n): ')
+    if(play_again == 'n' or play_again != 'y'):
+        print('Goodbye! Thank you for playing with us!')
+        break
+
+
+
+
---
+++ README.md
@@ -0,0 +1,9 @@
+#Cloud Computing 
+##Spring Semester 2018 - UPC
+
+###Proffesor:
+* Angel Toribio-González 
+
+###Group Members:
+* Ozge Koroglu - [ozgekoroglu@gmail.com](mailto:ozgekoroglou@gmail.com)
+* Sofia Yfantidou - [s.yfantidou@gmail.com](mailto:s.yfantidou@gmail.com)
