c6ff413b2496fee9f047cd75da07a48f1549e3ae
Sofia Yfantidou <syfantid@csd.auth.gr>
2018-03-02 12:34:27 +0100

    Finalize game logic and reformat file

--- Lab-1/lab1.guessnumber.py
+++ Lab-1/lab1.guessnumber.py
@@ -1,36 +1,58 @@
 import random
 
-# Allows multiple games
-while(True):
-    random_number = random.randint(1, 206)
-    number_guess=0
 
-    #Allows 5 guesses only
-    while(number_guess<5):
-        #Typed character must be number
-        try:
-            guess = int(input('Guess the number between 1 and 20: '))
-        except ValueError :
-            print("Plese give a number!")
-            continue
+# Checks whether a number is between a specified range
+def range_check(number, lower_bound, upper_bound):
+    if number > upper_bound or number < lower_bound:
+        print('Plese give a number in the range of ' + str(lower_bound) + ' and ' + str(upper_bound) + '.\n')
+        return False
+    return True
 
-        #Typed number must be in the range of 1 and 20
-        if(guess > 20 or guess < 1):
-            print("Plese give a number in the range of 1 and 20")
-            continue
-        number_guess += 1
-        if(guess == random_number):
-            print('Excellent guess! You succeeded in your '+ str(number_guess) + " try")
+
+# Game to guess a random number between a specified range
+def random_number_game(lower_bound, upper_bound):
+    # Allows multiple games
+    while True:
+        random_number = random.randint(lower_bound, upper_bound)
+        number_guess = 0
+
+        # Allows 5 guesses only
+        while number_guess < 5:
+
+            # Typed character must be number
+            try:
+                guess = int(input('Guess the number between ' + str(lower_bound) + ' and ' + str(upper_bound) + ': '))
+            except ValueError:
+                print('Plese give a number!\n')
+                continue
+
+            # Typed number must be in the range between lower bound and upper bound
+            if not range_check(guess, lower_bound, upper_bound):
+                continue
+
+            # Actual game logic
+            number_guess += 1
+            if guess == random_number:
+                print('Excellent guess! You succeeded in your ' + str(number_guess) + ' try!\n')
+                break
+            elif guess > random_number:
+                print('Sorry, number is too high. Try again!\n')
+            else:
+                print('Sorry, number is too low. Try again!\n')
+
+        if number_guess == 5:
+            print('Sorry you are out of tries. :(\n')
+
+        # Allow user to play again
+        play_again = input('Do you want to play again (y/n): ')
+        if play_again == 'n' or play_again != 'y':
+            print('Goodbye! Thank you for playing with us!\n')
             break
-        elif(guess > random_number):
-            print('Sorry, number is too high. Try again!')
-        else:
-            print('Sorry, number is too low. Try again!')
-    if(number_guess==5):
-        print('Sorry you are out of tries :(')
 
-    # Allow user to play again
-    play_again = input('Do you want to play again (y/n): ')
-    if(play_again == 'n' or play_again != 'y'):
-        print('Goodbye! Thank you for playing with us!')
-        break
+
+def main():
+    random_number_game(1, 20)
+
+
+if __name__ == "__main__":
+    main()
